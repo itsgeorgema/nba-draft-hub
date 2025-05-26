d64e021f161fd19dba9c8ca5037b490dd98212a0
itsgeorgema <georgema2020@gmail.com>
2025-05-25 21:33:14 -0700

    fix path to file

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function AppContent() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/data/intern_project_data.json');
+        const response = await fetch('../data/intern_project_data.json');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
